3747d927e855f5e0381c691d2afc339259528343
Aleksei Pirogov <alekseipirogovQA@yandex.ru>
2025-10-21 21:10:23 +0300

    Исправилено имя файла. Реализованы операции с числами: сумма, разность, произведение, частное

---
+++ src/AccessLogParser.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AccessLogParser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+
+        scanner.close();
+    }
+}
