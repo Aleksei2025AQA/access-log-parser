c49e5a40bb0119f9f2ab8d77d827cf250e16692e
Aleksei Pirogov <alekseipirogovQA@yandex.ru>
2026-01-15 13:00:24 +0300

    Созданий доп. классов

---
+++ src/HttpMethod.java
@@ -0,0 +1,11 @@
+public enum HttpMethod {
+    GET, POST, PUT, DELETE, HEAD, OPTIONS, PATCH, CONNECT, TRACE;
+
+    public static HttpMethod fromString(String method) {
+        try {
+            return HttpMethod.valueOf(method);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+}
