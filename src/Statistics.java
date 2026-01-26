e2d4fbc3a7553248f95c6563d4a2a59a836895d6
Aleksei Pirogov <alekseipirogovQA@yandex.ru>
2026-01-26 15:59:54 +0300

    Доработан класс Statistics

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,11 +1,17 @@
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Statistics {
 
     private int totalTraffic;
     private LocalDateTime minTime;
     private LocalDateTime maxTime;
+    private HashSet<String> existingPages = new HashSet<>();
+    private HashMap<String, Integer> osCount = new HashMap<>();
 
     public Statistics() {
         this.totalTraffic = 0;
@@ -14,6 +20,7 @@ public class Statistics {
     }
 
     public void addEntry(LogEntry entry) {
+
         totalTraffic += entry.getResponseSize();
 
         LocalDateTime entryTime = entry.getTime();
@@ -23,18 +30,30 @@ public class Statistics {
         if (maxTime == null || entryTime.isAfter(maxTime)) {
             maxTime = entryTime;
         }
+
+        if (entry.getResponseCode() == 200) {
+            existingPages.add(entry.getPath());
+        }
+
+
+        UserAgent userAgent = entry.getUserAgent();
+        if (userAgent != null) {
+            String osType = userAgent.getOsType();
+            if (osType != null && !osType.isEmpty()) {
+                osCount.put(osType, osCount.getOrDefault(osType, 0) + 1);
+            }
+        }
     }
+
 
     public double getTrafficRate() {
         if (minTime == null || maxTime == null || minTime.equals(maxTime)) {
             return 0.0;
         }
-
         long hours = Duration.between(minTime, maxTime).toHours();
         if (hours == 0) {
-            hours = 1; // чтобы избежать деления на ноль
+            hours = 1;
         }
-
         return (double) totalTraffic / hours;
     }
 
@@ -48,5 +67,29 @@ public class Statistics {
 
     public LocalDateTime getMaxTime() {
         return maxTime;
+    }
+
+    public Set<String> getAllExistingPages() {
+        return new HashSet<>(existingPages);
+    }
+
+    public Map<String, Double> getOperatingSystemStatistics() {
+        Map<String, Double> osStatistics = new HashMap<>();
+
+        if (osCount.isEmpty()) {
+            return osStatistics;
+        }
+
+        int total = 0;
+        for (int count : osCount.values()) {
+            total += count;
+        }
+
+        for (Map.Entry<String, Integer> entry : osCount.entrySet()) {
+            double share = (double) entry.getValue() / total;
+            osStatistics.put(entry.getKey(), share);
+        }
+
+        return osStatistics;
     }
 }
